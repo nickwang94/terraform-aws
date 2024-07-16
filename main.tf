6b7ecc5314532be23e22624064bc2c88404068c6
NickWang <nick.wenkun.wang@outlook.com>
2024-07-16 21:14:48 +0800

    NO-JIRA: ocnfig internet gateway and route table

--- main.tf
+++ main.tf
@@ -23,3 +23,29 @@ resource "aws_subnet" "myapp-subnet-1" {
     Name: "${var.env_prefix}-subnet-1"
   }
 }
+
+# Route Table, like a virtual router
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name: "${var.env_prefix}-rtb"
+  }
+}
+
+# Terraform knows in which sequence the components must be created
+# Internet Gateway, like a virtual modem
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name: "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
