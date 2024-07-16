78169a7272c4700d274111fac4b22b33f03c7327
NickWang <nick.wenkun.wang@outlook.com>
2024-07-16 21:46:29 +0800

    NO-JIRA: Adding Security Group

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip {}
 
 
 resource "aws_vpc" "myapp-vpc" {
@@ -48,4 +49,36 @@ resource "aws_internet_gateway" "myapp-igw" {
 resource "aws_route_table_association" "a-rtb-subnet" {
   subnet_id = aws_subnet.myapp-subnet-1.id
   route_table_id = aws_route_table.myapp-route-table.id
+}
+
+# Security Grouo
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+  # Incoming Triffic
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name: "${var.env_prefix}-sg"
+  }
 }
